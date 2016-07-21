57462c4580c0175616f6ba5bc03223dce3b1c127
Aaron Smith <aaron@marinosoftware.com>
2016-07-21 10:38:11 +0100

    Attempting to fix OOM error by clearing arrays when they get too big

--- logcatviewer/src/main/java/com/fatangare/logcatviewer/ui/adapter/LogcatViewerListAdapter.java
+++ logcatviewer/src/main/java/com/fatangare/logcatviewer/ui/adapter/LogcatViewerListAdapter.java
@@ -285,6 +285,12 @@ public class LogcatViewerListAdapter extends BaseAdapter {
      * @param logcatEntry
      */
     private void addFilterLogcatEntry(String logcatEntry) {
+        // Clear arrays if the amount of entries gets too big, otherwise we eventually get OOM
+        if (mFilteredLogcatData.size() > 10000) {
+            mFilteredLogcatData.clear();
+            mLogcatData.clear();
+        }
+        
         if ((TextUtils.isEmpty(mLogPriorityLevel) || (!TextUtils.isEmpty(mLogPriorityLevel) && priorityLevelConditionForFiltering(logcatEntry))) &&
                 (TextUtils.isEmpty(mLogFilterText) || (!TextUtils.isEmpty(mLogFilterText) &&
                         logcatEntry.toLowerCase().contains(mLogFilterText.toLowerCase())))) {
